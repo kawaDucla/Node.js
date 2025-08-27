d7e0b6e3787b6a61e611191695baea1f8a6be917
Kawã Felipe Ducla Aguiar <kawafelipeducla01@gmail.com>
2025-08-27 15:14:51 -0300

    atualizado

---
+++ Frontend/cadastroUsuario/script.js
@@ -0,0 +1,21 @@
+    
+
+fetch('http://localhost:3000', {
+
+    method: 'POST',
+
+    headers: {
+
+        'Content-Type': 'application/json'
+
+    },
+
+    body: JSON.stringify(data)
+
+})
+
+    .then(response => response.json())
+
+    .then(data => console.log(data))
+
+    .catch(error => console.log(error));
